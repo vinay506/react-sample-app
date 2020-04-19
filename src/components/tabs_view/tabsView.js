c8f751567edc82bff4f483445f3948d20487b9f3
vinay <vinay.vin506@gmail.com>
2020-04-19 20:33:43 +0530

    code changes for tab view

--- src/components/tabs_view/tabsView.js
+++ src/components/tabs_view/tabsView.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
+import './tabsViewStyles.scss';
 
 function TabsView() {
 
-    const tabs = ["tab1", "tab2"];
+    const tabs = [{name:"tab1"},{name:"tab2"}];
+    const [currentTab,setCurrentTab] = useState({name:"tab1"});
 
     function getTabs() {
         return (
             <div className="tab">
                 {
                     tabs.map(tab => {
-                        return <button key={tab} className="tabItem" onClick={()=>{onTabChange(tab)}}>{tab}</button>
+                        return <button key={tab.name} className={currentTab.name === tab.name ? 'active':'inactive'} onClick={()=>{onTabChange(tab)}}>{tab.name}</button>
                     })
                 }
             </div>
@@ -18,6 +20,7 @@ function TabsView() {
 
     function onTabChange(tab){
         console.log(tab);
+        setCurrentTab(tab);
     }
 
     function getTabsConent() {
@@ -29,7 +32,7 @@ function TabsView() {
     }
 
     function getTabContent(tab){
-        return <div id={tab}>{tab}</div>
+        return <div id={tab.name} className={currentTab.name === tab.name ? 'active':'inactive'}>{tab.name}</div>
     }
 
     return (
