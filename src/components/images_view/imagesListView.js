9345a55fed5e6cadae9e64bd2565933ae20d94d2
vinay <vinay.vin506@gmail.com>
2020-04-19 16:45:35 +0530

    Integrated submit search val

--- src/components/images_view/imagesListView.js
+++ src/components/images_view/imagesListView.js
@@ -2,23 +2,34 @@ import React,{useEffect ,useState} from 'react';
 import SearchBox from '../search-box/search-box';
 import apiService from '../apiService'
 import ImageList from '../image_list/ImageList'
-function ImagesListView() {
+function ImagesListView(props) {
 
     const [images,setImages] = useState([]);
+    const [searchVal,setSearchVal] = useState('');
 
     const fetchImages =  async () => {
         const response =  await apiService.get('/search/photos',{
-              params:{query:'cars'}
+              params:{query:searchVal}
           });
           setImages(response.data.results);
        }
 
+    const onSubmitSearchVal = (value)=>{
+        if(value){
+            setSearchVal(value);
+            fetchImages();
+        }
+    }
+
     useEffect(() => {
-       fetchImages()
-    }, [])
+        if(props.searchVal){
+            fetchImages()
+        }
+    }, [props.searchVal])
+
     return (
         <div>
-            <SearchBox/>
+            <SearchBox onSubmit={onSubmitSearchVal}/>
             <ImageList images={images}/>
         </div>
     )
--- src/components/search-box/search-box.js
+++ src/components/search-box/search-box.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React ,{useRef}from 'react';
+import './search-box-styles.scss';
 // import {  MDBIcon } from "mdbreact";
 
 function SearchBox(props) {
     console.log('In search Box',props);
+    const inputRef =useRef(null);
+    function submitSearchVal(){
+        const value = inputRef.current.value;
+        props.onSubmit(value);
+    }
+
     return (
-        <div>
-            <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
-            <button>Search</button>
+        <div className="search-box">
+            <input ref={inputRef} className="form-control" type="text" placeholder="Search" aria-label="Search" />
+            <button onClick={()=>{submitSearchVal()}}>Search</button>
         </div>
     )
 }
