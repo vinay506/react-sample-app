e208cb46b1ce4a823c65fc4351acc812544c8028
vinay <vinay.vin506@gmail.com>
2020-04-19 19:49:47 +0530

    code changes for tabsView

--- src/components/tabs_view/tabsView.js
+++ src/components/tabs_view/tabsView.js
@@ -1,10 +1,53 @@
 import React from 'react'
 
 function TabsView() {
+
+    const tabs = ["tab1", "tab2"];
+
+    function getTabs() {
+        return (
+            <div className="tab">
+                {
+                    tabs.map(tab => {
+                        return <button key={tab} className="tabItem" onClick={()=>{onTabChange(tab)}}>{tab}</button>
+                    })
+                }
+            </div>
+        )
+    }
+
+    function onTabChange(tab){
+        console.log(tab);
+    }
+
+    function getTabsConent() {
+        return <div className="tab-content">
+            {tabs.map(tab=>{
+                return getTabContent(tab)
+            })}
+        </div>
+    }
+
+    function getTabContent(tab){
+        return <div id={tab}>{tab}</div>
+    }
+
     return (
         <div>
-            Tabs View      
+            <div>
+                <select className="form-controll">
+                    <option value="first">First</option>
+                    <option value="second">Second</option>
+                </select>
+
+            </div>
+            <div>
+                {getTabs()}
+                {getTabsConent()}
+            </div>
+
         </div>
+
     )
 }
 
